5d01a36cf4011f5dfe5452027ade16be273991ba
Karishmamaddimsetti <93146523+Karishmamaddimsetti@users.noreply.github.com>
2021-10-27 15:29:05 +0530

    Add files via upload

---
+++ tm of stud.c
@@ -0,0 +1,23 @@
+//printing total marks of the student in diff subjects
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int phy,chem,math,eng,tm;
+	//intialisation
+	phy =50;
+	chem =60;
+	math =70;
+	eng =40;
+	//calculation
+	tm = phy+chem+math+eng;
+	//output
+	
+	printf("marks in phy is%d\n",phy);
+	printf("marks in chem is%d\n",chem);
+	printf("marks in math is%d\n",math);
+	printf("marks in eng is%d\n",eng);
+	printf(" total marks is%d\n",tm);
+}
+	
+
