0cf59c61657d658a69238a46f8ae1f948697d69b
Karishmamaddimsetti <93146523+Karishmamaddimsetti@users.noreply.github.com>
2021-10-30 19:10:59 +0530

    Add files via upload

---
+++ fibonacci series with recursion.c
@@ -0,0 +1,26 @@
+//printing fibonacci series with recursion
+#include<stdio.h>
+void printFibonacci(int n){
+static int n1=0,n2=1,n3;
+
+	if(n>0)
+	{
+		n3 =n1+n2;
+		n1 =n2;
+		n2 =n3;
+		printf("%d",n3);
+		printFibonacci(n-1);
+	}
+}
+
+int main()
+
+{
+	int n;
+	printf("enter number of elements :");
+	scanf("%d",&n);
+	printf("fibonacci series :");
+	printf("\n%d %d",0,1);//printing 0 and 1
+	printFibonacci(n-2);//n-2 because 2 numbers  are already printed
+}
+	
