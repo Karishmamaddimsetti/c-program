a1f01b588e90d163e5afd861baba7dbfff0f849e
Karishmamaddimsetti <93146523+Karishmamaddimsetti@users.noreply.github.com>
2021-11-08 12:06:31 +0530

    Add files via upload

---
+++ conditional operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a=20,b =10;
+	printf(" 20>10 --> %d\n",a > b);
+	printf(" 20>=10 --> %d\n",a >= b);
+	printf(" 20<10  --> %d\n",a < b);
+	printf(" 20<=10 --> %d\n",a <= b);
+	printf(" 20==10 --> %d\n",a == b);
+	printf(" 20!=10 --> %d\n",a != b);
+	
+	
+	
+}
