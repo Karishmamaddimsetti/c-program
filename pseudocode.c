3813d37d428184ef4bf8221e72f2118a58b2082a
Karishmamaddimsetti <93146523+Karishmamaddimsetti@users.noreply.github.com>
2021-12-15 20:47:52 +0530

    Add files via upload

---
+++ pseudocode.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int p,q,r;
+	printf("enter value :");
+	scanf("%d%d%d",&p,&q,&r);
+	if((1+r+p)>(p+q))
+	
+	{
+		p =5+r;
+		p =(9+12)+q;
+		
+	}
+	
+	{
+		printf("%d",p+q+r);
+	}
+}
